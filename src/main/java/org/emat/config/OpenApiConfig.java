4819acf4ebb63d8af9bdeff936f7e1465408143a
saic98 <134575105+saic98@users.noreply.github.com>
2026-08-13 07:42:59 +0530

    Changes

--- src/main/java/org/emat/config/OpenApiConfig.java
+++ src/main/java/org/emat/config/OpenApiConfig.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
 @Configuration
+@Profile({"dev", "local", "docs"})
 public class OpenApiConfig {
 
     @Bean
