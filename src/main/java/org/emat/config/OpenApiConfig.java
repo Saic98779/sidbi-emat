4b19ce1ea9bb817eb27b89f6514005d1dbc62318
saic98 <134575105+saic98@users.noreply.github.com>
2026-09-15 17:34:30 +0530

    Vault configuration

--- src/main/java/org/emat/config/OpenApiConfig.java
+++ src/main/java/org/emat/config/OpenApiConfig.java
@@ -9,10 +9,8 @@ import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"dev", "local", "docs"})
 public class OpenApiConfig {
 
     @Bean
